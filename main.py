429f4166f02ca2de3a3f734d935f2853964baf5e
Viperack <theok04@gmail.com>
2022-03-23 22:17:14 +0100

    Added days detection

---
+++ main.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+def day(dayOfWeek):
+    match dayOfWeek:
+        case 0:
+            return "Monday"
+        case 1:
+            return "Tuesday"
+        case 2:
+            return "Wednesday"
+        case 3:
+            return "Thursday"
+        case 4:
+            return "Friday"
+        case 5:
+            return "Saturday"
+        case 6:
+            return "Sunday"
+        case _:        
+            return "Incorrect day"
+
+print (day(datetime.today().weekday()))

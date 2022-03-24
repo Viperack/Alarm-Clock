f5161cd63824d09f3c9ec0ff7ccd8017384ee2c7
Viperack <theok04@gmail.com>
2022-03-25 00:36:46 +0100

    Added time control

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+import time
+
 import RPi.GPIO as GPIO
 
 pinPiezo = 21
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -9,15 +12,50 @@ GPIO.setup(pinPiezo, GPIO.OUT)
 
 GPIO.output(pinPiezo, GPIO.LOW)
 
-def day(dayOfWeek):
-    return {
-        0: "Monday",
-        1: "Tuesday",
-        2: "Wednesday",
-        3: "Thursday",
-        4: "Friday",
-        5: "Saturday",
-        6: "Sunday",
-    }[dayOfWeek]
+def alarm(day):
+    day
 
-#print (day(datetime.today().weekday()))
+    if time.localtime().tm_hour >= wake_up_times[day][0]:
+        if time.localtime().tm_hour == wake_up_times[day][0]:
+            if time.localtime().tm_min < wake_up_times[day][1]:
+                time.sleep(60 * (wake_up_times[day][1] - time.localtime().tm_min))
+            else:
+                time.sleep(3600 * (23 - time.localtime().tm_hour) + 60 * (60 - time.localtime().tm_min))
+        else:
+            time.sleep(3600 * (23 - time.localtime().tm_hour) + 60 * (60 - time.localtime().tm_min))
+    else:    
+        time.sleep(60 * (60 - time.localtime().tm_min))
+
+    return 0
+
+def turnInt(string):
+
+    return int(string)
+
+def importWakeUpData(fileName):
+
+    file = open(fileName, "r")
+    separatedDays = file.read().split(",")
+    separatedMinHour = []
+
+    for i in separatedDays:
+
+        separatedMinHour.append(list(map(turnInt, i.split(":"))))
+
+    return separatedMinHour
+
+
+wake_up_times = importWakeUpData("wake_up_time.txt")
+
+
+# print (wake_up_times)
+
+# print (day(datetime.today().weekday()))
+# print (time.localtime().tm_hour)
+while (True):
+        alarm(datetime.today().weekday())
+        while (True):
+            GPIO.output(pinPiezo, GPIO.HIGH)
+            time.sleep(0.4)
+            GPIO.output(pinPiezo, GPIO.LOW)
+            time.sleep(0.2)
